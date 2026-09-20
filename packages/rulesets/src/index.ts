1ee89940a9a99b6d083d4fa31807796ef8a885ea
Christopher Young <chrisyoungplus@outlook.com>
2026-09-20 11:59:19 -0500

    0.6.0-D: register outpost ruleset

--- packages/rulesets/src/index.ts
+++ packages/rulesets/src/index.ts
@@ -24,6 +24,7 @@ import { classicOgV05F } from './classic-og-v0.5-f/index.js';
 import { classicOgV06A } from './classic-og-v0.6-a/index.js';
 import { classicOgV06B } from './classic-og-v0.6-b/index.js';
 import { classicOgV06C } from './classic-og-v0.6-c/index.js';
+import { classicOgV06D } from './classic-og-v0.6-d/index.js';
 import type { Ruleset } from './types.js';
 
 export { classicOgV01 };
@@ -52,6 +53,7 @@ export { classicOgV05F };
 export { classicOgV06A };
 export { classicOgV06B };
 export { classicOgV06C };
+export { classicOgV06D };
 export * from './classic-og-v0.1/index.js';
 export * from './types.js';
 export * from './combat-prototype.js';
@@ -84,6 +86,7 @@ export const rulesets: Readonly<Record<string, Ruleset>> = {
   [classicOgV06A.meta.id]: classicOgV06A,
   [classicOgV06B.meta.id]: classicOgV06B,
   [classicOgV06C.meta.id]: classicOgV06C,
+  [classicOgV06D.meta.id]: classicOgV06D,
 };
 
 export const DEFAULT_RULESET_ID = classicOgV01.meta.id;
